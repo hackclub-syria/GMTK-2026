b734555ff1fe2f30af9fb40efe0722e9b018ffb9
Sir-Lime <nabelaltabaa@gmail.com>
2026-07-24 04:17:29 +0300

    lerp

--- Assets/Scripts/cursor_script.cs
+++ Assets/Scripts/cursor_script.cs
@@ -8,13 +8,16 @@ public class cursor_script : MonoBehaviour
 {
     [SerializeField] private Transform T_mask;
     [SerializeField] private float tempo = 120;
-    [SerializeField] private int[] pattern = new int[] { 4, 2, 4, 2 }; 
+    [SerializeField] private int[] pattern = new int[] { 4, 2, 4, 2 };
+    [SerializeField] private float lerpSpeed = 15f;
+
     private Camera Cam;
     private float interval;
     private float timer = 0f;
 
     private int ind_pattern = 0;
     private int currentBeat;
+    private Vector3 target_pos;
 
     void Start()
     {
@@ -30,12 +33,18 @@ public class cursor_script : MonoBehaviour
         }
 
         currentBeat = pattern[0];
+
+        if (T_mask != null)
+        {
+            target_pos = T_mask.localPosition;
+        }
     }
 
     void Update()
     {
         Replace_cursor();
         Handle_countdown();
+        T_mask.localPosition = Vector3.Lerp(T_mask.localPosition, target_pos, Time.deltaTime * lerpSpeed);
     }
 
     void Handle_countdown()
@@ -47,15 +56,18 @@ public class cursor_script : MonoBehaviour
             timer -= interval;
             currentBeat--;
             int length = pattern[ind_pattern];
+
             if (currentBeat == length - 1)
             {
-                T_mask.localPosition = new Vector3(T_mask.localPosition.x, 0.1f, T_mask.localPosition.z);
+                target_pos = new Vector3(target_pos.x, 0.1f, target_pos.z);
             }
-            T_mask.localPosition = new Vector3(
-                T_mask.localPosition.x,
-                T_mask.localPosition.y - (T_mask.localScale.y / length),
-                T_mask.localPosition.z
+
+            target_pos = new Vector3(
+                target_pos.x,
+                target_pos.y - (T_mask.localScale.y / length),
+                target_pos.z
             );
+
             if (currentBeat > 0)
             {
                 Debug.Log(currentBeat);
@@ -68,6 +80,7 @@ public class cursor_script : MonoBehaviour
             }
         }
     }
+
 
     void Replace_cursor()
     {
