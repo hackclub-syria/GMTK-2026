ce8512959bb69e9b5f61b38439fbc53f73615eb8
Sir-Lime <nabelaltabaa@gmail.com>
2026-07-26 06:41:56 +0300

    added spike

--- Assets/Scripts/Die.cs
+++ Assets/Scripts/Die.cs
@@ -3,16 +3,30 @@ using UnityEngine;
 
 public class Die : MonoBehaviour
 {
+    public static Die instance { get; private set; }
+    [Header("Settings")]
     public GameObject deathVfxPrefab;
+
     private SpriteRenderer duckSprite;
     private Collider2D duckCollider;
 
-    void Start()
+    private void Awake()
     {
-        //Invoke("DieBih", 2f);
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+    }
+
+    private void Start()
+    {
         duckSprite = GetComponent<SpriteRenderer>();
         duckCollider = GetComponent<Collider2D>();
     }
+
     public void DieBih()
     {
         StartCoroutine(JuicyDeathRoutine());
@@ -22,13 +36,19 @@ public class Die : MonoBehaviour
     {
         if (duckCollider != null) duckCollider.enabled = false;
         if (duckSprite != null) duckSprite.enabled = false;
-        Instantiate(deathVfxPrefab, transform.position, Quaternion.identity);
-        
-        // impact frame ahh
+        if (deathVfxPrefab != null)
+        {
+            Instantiate(deathVfxPrefab, transform.position, Quaternion.identity);
+        }
         Time.timeScale = 0f;
         yield return new WaitForSecondsRealtime(0.1f);
         Time.timeScale = 1f;
-        
+
         Destroy(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        Time.timeScale = 1f;
     }
 }
