a803c4c02ed87c4e364f1e2bbb6721d1779b4551
Kazoya - Zaid Mermam <mermamzaid@gmail.com>
2026-07-26 03:34:26 +0300

    DUH NOW DIE

---
+++ Assets/Scripts/Die.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using UnityEngine;
+
+public class Die : MonoBehaviour
+{
+    public GameObject deathVfxPrefab;
+    private SpriteRenderer duckSprite;
+    private Collider2D duckCollider;
+
+    void Start()
+    {
+        Invoke("DieBih", 2f);
+        duckSprite = GetComponent<SpriteRenderer>();
+        duckCollider = GetComponent<Collider2D>();
+    }
+    public void DieBih()
+    {
+        StartCoroutine(JuicyDeathRoutine());
+    }
+
+    private IEnumerator JuicyDeathRoutine()
+    {
+        if (duckCollider != null) duckCollider.enabled = false;
+        if (duckSprite != null) duckSprite.enabled = false;
+        Instantiate(deathVfxPrefab, transform.position, Quaternion.identity);
+        
+        // impact frame ahh
+        Time.timeScale = 0f;
+        yield return new WaitForSecondsRealtime(0.05f);
+        Time.timeScale = 1f;
+        
+        Destroy(gameObject);
+    }
+}
