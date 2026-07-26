e57cf969ffc71a080c914918f1a4aa5b3a4312c4
Kazoya - Zaid Mermam <mermamzaid@gmail.com>
2026-07-26 15:28:44 +0300

    tutorial done~ and fixed duck prefab issue

--- Assets/Scripts/EmoRunAwayFromParents.cs
+++ Assets/Scripts/EmoRunAwayFromParents.cs
@@ -5,5 +5,6 @@ public class EmoRunAwayFromParents : MonoBehaviour
     void Start()
     {
         transform.SetParent(null, true);
+        transform.localScale = Vector3.one; // the canvas fucks the scale in the first frame
     }
 }
