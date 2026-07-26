60545354e77be3f57a2b2db3fefc251b47f11e83
Kazoya - Zaid Mermam <mermamzaid@gmail.com>
2026-07-26 07:45:47 +0300

    u can now restart gaem when ded

--- Assets/Scripts/Die.cs
+++ Assets/Scripts/Die.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class Die : MonoBehaviour
 {
     public GameObject deathVfxPrefab;
+    public GameObject restartMenu;
+    public GameObject pauseMenu;
     private SpriteRenderer duckSprite;
     private Collider2D duckCollider;
 
     void Start()
     {
-        //Invoke("DieBih", 2f);
+        Invoke("DieBih", 2f);
         duckSprite = GetComponent<SpriteRenderer>();
         duckCollider = GetComponent<Collider2D>();
     }
@@ -20,6 +24,8 @@ public class Die : MonoBehaviour
 
     private IEnumerator JuicyDeathRoutine()
     {
+        restartMenu.SetActive(true);
+        pauseMenu.SetActive(false);
         if (duckCollider != null) duckCollider.enabled = false;
         if (duckSprite != null) duckSprite.enabled = false;
         Instantiate(deathVfxPrefab, transform.position, Quaternion.identity);
