05959cbaddb5a8b1a2053dadfb9a78adf8fe357a
Kazoya - Zaid Mermam <mermamzaid@gmail.com>
2026-07-26 17:29:31 +0300

    pushing

--- Assets/Scripts/EmoRunAwayFromParents.cs
+++ Assets/Scripts/EmoRunAwayFromParents.cs
@@ -2,9 +2,14 @@ using UnityEngine;
 
 public class EmoRunAwayFromParents : MonoBehaviour
 {
+    public bool isBitch = false;
     void Start()
     {
         transform.SetParent(null, true);
         transform.localScale = Vector3.one; // the canvas fucks the scale in the first frame
+        if (isBitch)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
     }
 }
